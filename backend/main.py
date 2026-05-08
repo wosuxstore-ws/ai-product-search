e1982de41698b4f3ed91252882b460608ed44a37
wosuxstore-ws <wosux.store@gmail.com>
2026-05-07 21:17:54 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 model, _, preprocess = open_clip.create_model_and_transforms(
     'ViT-B-32',
-    pretrained='laion2b_s34b_b79k'
+    pretrained='openai'
 )
 
 # =========================
